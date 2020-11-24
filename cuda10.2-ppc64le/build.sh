174f732aed2b7cb289d6d1ceed4b30c281751c83
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-11-24 08:54:39 -0800

    tag as 10.2-cudnn7-tensorrt7-nccl2

--- cuda10.2-ppc64le/build.sh
+++ cuda10.2-ppc64le/build.sh
@@ -4,4 +4,4 @@ REGISTRY=${REGISTRY:-gcr.io/ecp-e4s}
 
 DOCKER_CLI_EXPERIMENTAL=enabled \
 DOCKER_BUILDKIT=1 \
- docker build -t "${REGISTRY}/ubuntu18.04-cuda-ppc64le:10.2-cudnn7-tensorrt7-nccl2-tmp" .
+ docker build -t "${REGISTRY}/ubuntu18.04-cuda-ppc64le:10.2-cudnn7-tensorrt7-nccl2" .
