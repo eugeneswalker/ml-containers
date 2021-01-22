5a06bdeb0b88b6076265bf793c09b50cceb1a4cf
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-01-22 00:08:57 -0500

    add tf 2.4 x86_64 recipe

---
+++ tensorflow-2.4-cuda11.2-x86_64/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+REGISTRY=${REGISTRY:-gcr.io/ecp-e4s}
+
+DOCKER_CLI_EXPERIMENTAL=enabled \
+DOCKER_BUILDKIT=1 \
+ docker build -t "${REGISTRY}/ubuntu18.04-tensorflow-x86_64:2.4-cuda11.2-cudnn8-tensorrt7-nccl2" .
